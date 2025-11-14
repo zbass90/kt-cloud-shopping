581054612fb4442eea28cba61c1b8d3ef5fa8a58
zbass90 <smilejkh90@gmail.com>
2025-11-14 23:55:29 +0900

    feat: redis configuration 구성

---
+++ src/main/java/com/kt/config/RedisConfiguration.java
@@ -0,0 +1,26 @@
+package com.kt.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfiguration {
+
+	private final RedisProperties redisProperties;
+	private static final String REDISSON_ADDRESS_FORMAT = "redis://%s:%s";
+
+	@Bean
+	public RedissonClient redissonClient() {
+		Config config = new Config();
+		String uri = String.format(REDISSON_ADDRESS_FORMAT, redisProperties.getHost(), redisProperties.getPort());
+		config.useSingleServer().setAddress(uri);
+		return Redisson.create(config);
+	}
+}
